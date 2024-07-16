12d7176792998d939173328c53f0695d34ebd7e3
sdeshmukh20 <satyajeet.deshmukh08@gmail.com>
2024-07-16 21:06:13 +0530

    section4-springsecsection4

--- springsecuritybasic/src/main/java/com/eazybytes/controller/LoginController.java
+++ springsecuritybasic/src/main/java/com/eazybytes/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.eazybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,16 @@ public class LoginController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
@@ -33,5 +39,6 @@ public class LoginController {
         }
         return response;
     }
+
 
 }
